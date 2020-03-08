9fd27867445a6c32059f386abcf1aed0db451002
JulienQu <59086294+JulienQu@users.noreply.github.com>
2020-03-08 14:52:16 +0100

    Update mode.py

--- mode.py
+++ mode.py
@@ -1,8 +1,8 @@
 from enum import Enum,auto
 class Mode(Enum):
-	decouverte= auto()
-	revision = auto()
-	remise = auto()
+	decouverte= auto() # Exo de difficulté 1 voire 2 par compétences. Difficulté augmente un peu au fur et à mesure
+	revision = auto() # Exo dont la difficulté des compétences est égale au niveau de l'élève sur ces compétences
+	remise = auto() 
 	
 	 def describe(self):
 		 return self.name, self.value
