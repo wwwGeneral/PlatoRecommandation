c96e2f0120ced38c26d977a0aa4b89931b41f7ad
NathanSaucet <nathans77340@gmail.com>
2020-03-16 15:08:03 +0100

    Update main loop

--- projet.py
+++ projet.py
@@ -3,7 +3,7 @@ import meta_bdd
 from Student import *
 from mode import Mode
 
-def next_exercice(dico,student,goal,mode):
+def next_exercice(dico,student,mode):
 	if (mode == Mode.decouverte):
 		for key in dico_meta.keys():
 			tag = dico_meta[key]['tag']
@@ -13,26 +13,37 @@ def next_exercice(dico,student,goal,mode):
 	elif (mode == Mode.revision):
 		pass
 	elif (mode == Mode.remise):
-		pass
-	
-			
+		pass	
 	pass
 	#Gros algorithme permettant de recommander un exercice 
 
 if __name__ == '__main__':
-	goal = {'program':1}
-	s = Student(50,100,0.1,dict())
+	skills = list()
+	skills.append({'program':1, 'function':1})
+	skills.append({'string':1, 'array':1, 'program':4})
+	skills.append({})
+	skills.append({'string':1, 'array':1, 'program':1,'function':1, 'variable':1})
+
+	students = list()
+	students.append(Student(30,75,0,skills[2]))
+	students.append(Student(50,100,1,skills[0]))
+	students.append(Student(50,100,0.5,skills[1]))
+	students.append(Student(80,100,0.3,skills[3]))
+
 	dico_meta = meta_bdd.meta
-	for dico in dico_meta.values():
-		print(dico)
-	while(True):
-		skills = next_exercice(dico_meta,s,goal,Mode.decouverte)
-		mark = s.genMark()
-		refus = s.refuse()
-		if refus:
-			print("refusé")
-			break
-		s.updateProfil(skills,mark)
+
+	for s in students:
+		nb_exo = 20
+		while(nb_exo != 0):
+			nb_exo-=1
+			exo = next_exercice(dico_meta,s,Mode.decouverte)
+			mark = s.genMark()
+			refus = s.refuse()
+			if refus:
+				print("refusé")
+				break
+			s.updateProfil(exo,mark)
+			print("Note reçu : "+str(mark)+" | profil : ", s.profil)
 	
 		
 	
