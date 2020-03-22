e594a6291ed0496f3d6835da0afc921b79c9ecc6
DmuPro <59116747+DmuPro@users.noreply.github.com>
2020-03-22 17:08:25 +0100

    Decouverte_presque_fini

--- Exercice.py
+++ Exercice.py
@@ -1,3 +1,4 @@
+from Student import *
 class Exercice:
 	def __init__(self,path,author,title,subject,tag,prequesites):
 		self.path = path
@@ -7,25 +8,38 @@ class Exercice:
 		self.tag = tag
 		self.prequesites = prequesites
 	
-	def getPath():
+	def getPath(self):
 		return self.path
 		
-	def getAuthor():
+	def getAuthor(self):
 		return self.author
 		
-	def getTag():
+	def getTag(self):
 		return self.tag
 		
-	def getTitle():
+	def getTitle(self):
 		return self.title
 
-	def getPrequesites():
+	def getPrequesites(self):
 		return self.prequesites
 		
-	def getSubject():
+	def getSubject(self):
 		return self.subject
-		
-		
+	
+	def hasPrequesites(self,student):
+		profil = student.profil
+		if not self.prequesites:
+			return True
+		if profil.get(self.subject):
+			subj = profil[self.subject]['skills']
+			for p in self.prequesites.items():
+				if not subj.get(p[0]):
+					return False
+				if subj[p[0]]<p[1]:
+					return False
+		return False
+
+
 	def __str__(self):
 		return "Path = "+self.path+" Author = "+self.author+" Title = "+self.title
 	
--- Meta_donnee.py
+++ Meta_donnee.py
@@ -2,8 +2,8 @@ import meta_bdd
 from Exercice import *
 
 class Meta_donnee:
-	def __init__(self):
-		self.liste = list()
+	def __init__(self,liste):
+		self.liste = liste
 	
 	def add(self,exercice):
 		self.liste.append(exercice)
@@ -14,7 +14,9 @@ class Meta_donnee:
 			exercice = Exercice(dico[0],dico[1]['author'],dico[1]['title'],dico[1]['subject'],dico[1]['tag'],dico[1]['prerequisites'])
 			self.add(exercice)
 	
-	
-	
-			
-	
+	def getMeta(self):
+		return self.liste
+	def tag(self,tag,student):
+		for exo in self.liste:
+			if tag == exo.getTag() and exo.hasPrequesites(student):
+				return exo.getTag()
